959de4b7de242f86430dec62d035f36771807069
dinushchathurya <dinushchathurya21@gmail.com>
2021-10-02 22:22:11 +0530

    feat: Peering auth route endpoint in index.js

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 
 /* import required paramaters for database connection */
 const config_app = require('./config/app.js');
 const { databaseInitializer } = require('./libraries/database.js');
 
+const { auth_route } = require('./routes');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }))
+
 /* connect to database */
 databaseInitializer(config_app.app.database_uri);
+
+app.use('/api/v1/auth', auth_route);
 
 /* listen for requests */
 app.listen(3000, ()=> {
