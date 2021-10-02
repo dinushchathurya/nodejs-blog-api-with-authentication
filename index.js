9bde825744a972b80886dd7ca4f564a1cf6e5c63
dinushchathurya <dinushchathurya21@gmail.com>
2021-10-02 19:25:57 +0530

    feat: Application nconnect with MongoDB

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const app = express();
 
+/* import required paramaters for database connection */
+const config_app = require('./config/app.js');
+const { databaseInitializer } = require('./libraries/database.js');
+
+/* connect to database */
+databaseInitializer(config_app.app.database_uri);
+
+/* listen for requests */
 app.listen(3000, ()=> {
-    console.log('application running');
+    console.log('Application running on port 3000');
 })
 
