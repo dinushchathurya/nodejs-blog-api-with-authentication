6d4ca81bc3a6a21b558c7e653b3ce292647a2322
dinushchathurya <dinushchathurya21@gmail.com>
2021-10-03 08:08:48 +0530

    feat: Create routes endpoint for login user

--- routes/auth.js
+++ routes/auth.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const { AuthController } = require('../controllers');
 
 router.post('/register', AuthController.create_user);
+router.post('/login', AuthController.login_user);
 
 module.exports = router;
