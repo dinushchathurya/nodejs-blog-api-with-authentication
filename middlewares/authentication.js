367b7df6d39ceb94c38a547cd033be176cbe0e15
dinushchathurya <dinushchathurya21@gmail.com>
2021-10-03 15:10:27 +0530

    refactor: Refactor vauthenticationVerifier function

--- middlewares/authentication.js
+++ middlewares/authentication.js
@@ -5,16 +5,21 @@ const api_config = require("../config/api.js");
 const authenticationVerifier = (req, res, next) => {
 
     const authHeader = req.headers.token;
-
     if (authHeader) {
         const token = authHeader.split(' ')[1];
         jwt.verify(token, api_config.api.jwt_secret, (err, user) => {
-            if (err) res.status(401).json("Invalid token");
+            if (err) {
+                res.status(403).json({
+                    type: "error",
+                    message: ("Invalid token")
+                });
+                return;
+            }
             req.user = user;
-            next()
+            next();
         })
     } else {
-        return res.status(401).json("You are not authenticated");
+        return res.status(401).json("You are not authenticated and you need to log in!");
     }
 }
 
