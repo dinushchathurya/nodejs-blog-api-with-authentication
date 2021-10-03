75982d6c7dd40f18d2e755510d88122a801291a4
dinushchathurya <dinushchathurya21@gmail.com>
2021-10-03 08:07:59 +0530

    feat: Create controller function to login user

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,4 +1,5 @@
 const { validationResult } = require('express-validator');
+const bcrypt = require('bcrypt');
 
 const User = require('../models/User');
 
@@ -6,6 +7,7 @@ const AuthController = {
     
     /* create new user */
     async create_user(req, res) {
+
         const errors = validationResult(req);
 
         if (!errors.isEmpty()) {
@@ -35,6 +37,48 @@ const AuthController = {
                     message: err
                 });
             }    
+        }
+    },
+
+    /* user login */
+    async login_user(req, res) {
+
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(422).json({
+                type: 'error',
+                message: errors.array()
+            });
+        } else { 
+            try {
+                const { username } = req.body;
+                const user = await User.findOne({ username: username });
+                if (!user) {
+                    return res.status(401).json({
+                        type: 'error',
+                        message: 'No account is associated with the given username'
+                    });
+                }
+
+                const isMatch = await bcrypt.compare(req.body.password, user.password);
+                if (!isMatch) {
+                    return res.status(401).json({
+                        type: 'error',
+                        message: 'Invalid credentials'
+                    });
+                }
+
+                const { password, ...data } = user._doc;
+                res.status(200).json({
+                    type: 'sucesss',
+                    data
+                });
+            } catch (err) {
+                res.status(500).json({
+                    type: 'error',
+                    message: err
+                });
+            }
         }
     }
 };
