4d20a72f5b9912777eeb012c981d5a83b137f61b
dinushchathurya <dinushchathurya21@gmail.com>
2021-10-03 15:13:05 +0530

    refactor: Protected rotes with authenticationVerifier middleware

--- routes/post.js
+++ routes/post.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
+const { authenticationVerifier } = require('../middlewares/authentication');
 const { PostController } = require('../controllers');
 
-router.get('/', PostController.get_posts);
+router.get('/',PostController.get_posts);
 router.get('/:id', PostController.get_post);
-router.post('/', PostController.create_post);
-router.put('/:id',PostController.update_post);
-router.delete('/:id', PostController.delete_post);
+router.post('/', authenticationVerifier,PostController.create_post);
+router.put('/:id', authenticationVerifier,PostController.update_post);
+router.delete('/:id', authenticationVerifier, PostController.delete_post);
 
 module.exports = router;
