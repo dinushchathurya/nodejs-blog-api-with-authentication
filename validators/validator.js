5e714f7cfbacc8c56d2e7f8431e33d2d96c49877
dinushchathurya <dinushchathurya21@gmail.com>
2021-10-04 08:42:22 +0530

    feat: Validate all the fields in create user

--- validators/validator.js
+++ validators/validator.js
@@ -2,6 +2,19 @@ const { check } = require('express-validator');
 const User = require('../models/User');
 
 exports.createUser = [
+    check('username')
+        .trim()
+        .escape()
+        .not()
+        .isEmpty()
+        .withMessage('Username can not be empty!')
+        .custom(value => {
+            return User.findByUsername(value).then(user => {
+                if (user) {
+                    return Promise.reject('Username already exists');
+                }
+            })
+        }),
     check('email')
         .trim()
         .escape()
@@ -10,12 +23,18 @@ exports.createUser = [
         .withMessage('Email can not be empty!')
         .isEmail()
         .withMessage('Invalid email address!')
-        .normalizeEmail(),
-    check('email').custom(value => {
-        return User.findByEmail(value).then(user => {
-            if (user) {
-                return Promise.reject('E-mail already in use');
-            }
-        });
-    })
+        .normalizeEmail()
+        .custom(value => {
+            return User.findByEmail(value).then(user => {
+                if (user) {
+                    return Promise.reject('E-mail already exists');
+                }
+            })
+        }),
+    check('password')
+        .trim()
+        .escape()
+        .not()
+        .isEmpty()
+        .withMessage('Password can not be empty!')
 ];
